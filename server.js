512d5d0c445ba8c0253bea14e391305301c99c95
krishna krish <rockstar.katrick@gmail.com>
2017-08-24 18:36:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,10 +75,16 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+  res.send(counter.toString());
+});
+
 
 app.get('/:articleName',function(req, res){
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]))
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two',function(req, res){
