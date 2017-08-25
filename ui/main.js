f15cf2a1e97a8997c35b1785a178816045359360
krishna krish <rockstar.katrick@gmail.com>
2017-08-25 15:25:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,10 @@
-console.log('Loaded!');
-//change the text of the main-text div
-var element = document.getElementById('main-text');
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-element.innerHTML = 'New value';
 
-//move the image
-var img=document.getElementById('Img');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft +'px';
+button.onclick = function(){
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 }
-img.onclick = function(){
-    var interval = setInterval(moveRight,10);
-};
