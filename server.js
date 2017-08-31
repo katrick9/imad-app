2aa9ae4a8d189ab543d8c1276d6db4ac968cb352
krishna krish <rockstar.katrick@gmail.com>
2017-08-31 16:14:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,16 @@ app.get('/counter', function (req, res) {
     counter = counter + 1;
   res.send(counter.toString());
 });
+var names = [];
+app.get('/submit-name',function(req, res){
+    //Get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);//TODO
+    //JSON: javascript Object Notation
+    res.send(JSON.stringify(names));
+    
+});
 
 
 app.get('/:articleName',function(req, res){
@@ -103,16 +113,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names = [];
-app.get('/submit-name',function(req, res){
-    //Get the name from the request
-    var name = req.query.name;
-    
-    names.push(name);//TODO
-    //JSON: javascript Object Notation
-    res.send(JSON.stringify(names));
-    
-});
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
